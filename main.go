19b41873ac34ccd5b6ff66bc6edecdebf463b849
“arthurnagem” <arthurnagem24@gmail.com>
2026-02-24 15:41:30 -0300

    add healthz endpoint and serve static files under /app

--- main.go
+++ main.go
@@ -8,14 +8,18 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+	})
 
 	assetsHandler := http.StripPrefix(
-		"/assets/",
-		http.FileServer(http.Dir("./assets")),
+		"/app/",
+		http.FileServer(http.Dir(".")),
 	)
 
-	mux.Handle("/assets/", assetsHandler)
+	mux.Handle("/app/", assetsHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
